c5aa68264f4ae536396172d78d073bb1dd7694b1
zhangxingzhi <zhangxingzhizailushang@gmail.com>
2019-05-07 00:09:08 +0800

    Feature: add d5_choice_collect.py

---
+++ dishangyijiao/d5_choice_collect.py
@@ -0,0 +1,15 @@
+while True:
+  a = []
+  q1=input('请问甲，你认罪吗？请回答：认罪或者沉默  ')
+  q2=input('请问乙，你认罪吗？请回答：认罪或者沉默  ')
+  if len(a) < 3:
+    a.append([q1, q2])
+  if q1 =='认罪' and q2 =='认罪':
+    print ('两人都得判10年，唉，好好准备后事吧！')
+  elif q1=='认罪' and q2=='沉默':
+    print ('甲判1年，乙判20年')
+  elif q1=='沉默' and q2=='认罪':
+    print('甲判20年，乙判1年')
+  else:
+    print ('你们两人都判3年，皆大欢喜！')
+    break
