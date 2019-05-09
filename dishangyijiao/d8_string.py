2bb058257cfb5987c6480a30992b76a5724d759f
zhangxingzhi <zhangxingzhizailushang@gmail.com>
2019-05-10 00:57:46 +0800

    Feature: add d8_string.py

---
+++ dishangyijiao/d8_string.py
@@ -0,0 +1,10 @@
+text = '''
+You jump, I jump.
+My heart will go on.
+To make each day count.
+I'll never let go,I'll never let go,Jack.
+Outwardly,I was everything a well-brought up girl should be.Inside,I was screaming.
+Winning that ticket, it is the luckiest thing in my life, so I know you, know you really honored, very honored, you must help me, promise me to live, promise me you will not give up ... no matter what happens, Whatever the environment, Rose, promise me, do not forget.'''
+
+text_1 = text.lower().replace('jump', 'happy')
+# 后面的3步，明天再弄
