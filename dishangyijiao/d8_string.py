25b12f2f989d1a26bcf963a5b6d854203b8137eb
zhangxingzhi <zhangxingzhizailushang@gmail.com>
2019-05-11 00:23:44 +0800

    Refactor: d8_string.py

--- dishangyijiao/d8_string.py
+++ dishangyijiao/d8_string.py
@@ -1,3 +1,4 @@
+import re
 text = '''
 You jump, I jump.
 My heart will go on.
@@ -7,4 +8,9 @@ Outwardly,I was everything a well-brought up girl should be.Inside,I was screami
 Winning that ticket, it is the luckiest thing in my life, so I know you, know you really honored, very honored, you must help me, promise me to live, promise me you will not give up ... no matter what happens, Whatever the environment, Rose, promise me, do not forget.'''
 
 text_1 = text.lower().replace('jump', 'happy')
-# 后面的3步，明天再弄
+text_2 = re.sub(r'\b\S?no?\S\b', "", text)
+text_3 = text_2.swapcase()
+words = text_3.split()
+words.sort()
+for word in words:
+  print(word)
