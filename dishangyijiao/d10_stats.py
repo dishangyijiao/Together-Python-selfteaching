0dd76449653c854022126002b75c4213e23c0d2b
zhangxingzhi <zhangxingzhizailushang@gmail.com>
2019-05-15 23:19:43 +0800

    Refactor: d10_stats.py use Counter

--- dishangyijiao/d10_stats.py
+++ dishangyijiao/d10_stats.py
@@ -1,3 +1,4 @@
+import collections
 text_en = '''
 If we can only encounter each other rather than stay with each other then I wish we had never encountered
 '''
@@ -6,12 +7,18 @@ def stats_text_en(text_en):
   if isinstance(text_cn, str):
     # 转换为数组字符串
     words_en = text_en.split()
-    # 统计每个单词的出现次数
-    words_en_count = { word: words_en.count(word) for word in words_en }
-    # 按照单词出现次数的倒序排序
-    sorted_words_en = sorted(words_en_count.items(), key=lambda kv: kv[1], reverse=True)
-    # 将倒序的结果输出
-    print(sorted_words_en)
+    # # 统计每个单词的出现次数
+    # words_en_count = { word: words_en.count(word) for word in words_en }
+    # # 按照单词出现次数的倒序排序
+    # sorted_words_en = sorted(words_en_count.items(), key=lambda kv: kv[1], reverse=True)
+    # # 将倒序的结果输出
+    # print(sorted_words_en)
+
+    # 使用Python标准库中的Counter进行优化
+    cnt = collections.Counter()
+    for word in words_en:
+      cnt[word] += 1
+    print(cnt)
   raise ValueError('text is not a string')
 
 
@@ -22,10 +29,14 @@ def stats_text_cn(text_cn):
   if isinstance(text_cn, str):
     # 字符串转换为数组字符串
     words_cn = text_cn.split()
-    # 统计每个汉字的出现次数
-    words_cn_count = { word: words_cn.count(word) for word in words_cn }
-    # 按照汉字的出现次数倒序排序
-    sorted_words_cn = sorted(words_cn_count.items(), key=lambda kv: kv[1], reverse=True)
-    # 将倒序的结果输出
-    print(sorted_words_cn)
+    # # 统计每个汉字的出现次数
+    # words_cn_count = { word: words_cn.count(word) for word in words_cn }
+    # # 按照汉字的出现次数倒序排序
+    # sorted_words_cn = sorted(words_cn_count.items(), key=lambda kv: kv[1], reverse=True)
+    # # 将倒序的结果输出
+    # print(sorted_words_cn)
+    cnt = collections.Counter()
+    for word in words_cn:
+      cnt[word] += 1
+      print(cnt)
   raise ValueError('text is not a string')
