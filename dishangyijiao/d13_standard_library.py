3ea3d854b88c104b3c74a41907f264b810d640f4
zhangxingzhi <zhangxingzhizailushang@gmail.com>
2019-05-16 23:44:02 +0800

    Refactor: print most_common(100)

--- dishangyijiao/d13_standard_library.py
+++ dishangyijiao/d13_standard_library.py
@@ -1,17 +1,21 @@
 import json
 import pdb
-import collecitons
+import collections
+import re
 # 读取文件
 with open('/Users/zh/Together-Python-selfteaching/dishangyijiao/tang300.json', 'r') as myfile:
   data = myfile.read()
 # pdb.set_trace()
 # 解析文件
 obj = json.loads(data)
-
-# TODO 明天再处理中文词频输出
-    cnt = collections.Counter()
-    for word in words_cn:
-        cnt[word] += 1
-        print(cnt.most_common(count))
+# 转换为string
+fullStr = '_'.join([str(elem) for elem in obj ])
+# 找出所有中文
+text_cn = re.findall(r'[\u4e00-\u9fa5]+', fullStr)
+# 输出结果
+cnt = collections.Counter()
+for word in text_cn:
+  cnt[word] += 1
+print(cnt.most_common(100))
 
 
