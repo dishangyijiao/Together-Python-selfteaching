247b70fdb0b8467b41420e9774ee57046f714fb0
zhangxingzhi <zhangxingzhizailushang@gmail.com>
2019-05-21 22:14:01 +0800

    Refactor: d17_training.py

--- dishangyijiao/d17_training.py
+++ dishangyijiao/d17_training.py
@@ -1,16 +1,48 @@
 import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
 import matplotlib.pyplot as plt
+import requests
+from pyquery import PyQuery
+import jieba
+import collections
+import re
+from wxpy import *
 
-# d15的结果
-# words_ten = [('的', 1310), ('是', 473), ('一个', 407), ('我们', 307), ('我', 273), ('你', 218), ('了', 217), ('在', 211), ('有', 157), ('微信', 139)]
-objects = ('的', '是', '一个', '我们', '我', '你', '了', '在', '有', '微信')
-y_pos = np.arange(len(objects))
-performance = [1310, 473, 407, 307, 273, 218, 217, 211, 157, 139]
+bot = Bot()
+my_friends = bot.friends()
+@bot.register(my_friends, SHARING)
 
-plt.bar(y_pos, performance, align='center', alpha=0.5)
-plt.xticks(y_pos, objects)
-plt.ylabel('times')
-plt.title('words appear')
+def auto_reply(msg):
+  msg.reply('收到消息')
 
-plt.show()
+  response = requests.get(msg.url)
+  document = PyQuery(response.text)
+  content = document('#js_content').text()
+  document_list = re.findall(r'[\u4e00-\u9fa5]+', content)
+  document_string = "".join(document_list)
+  content_list = jieba.cut(document_string, cut_all=False)
+  document_cn = " ".join(content_list)
+  words_cn = document_cn.split()
+
+  cnt = collections.Counter()
+  for word in words_cn:
+    cnt[word] += 1
+  result = cnt.most_common(10)
+  objects = []
+  performance = []
+  for word in result:
+    objects.append(word[0])
+    performance.append(word[1])
+
+  # 生成图片
+  y_pos = np.arange(len(objects))
+  plt.bar(y_pos, performance, align='center', alpha=0.5)
+  plt.xticks(y_pos, objects)
+  plt.ylabel('times')
+  plt.title('words appear')
+
+  # 保存为png格式,并对图片进行处理
+  plt.savefig('foo.png', bbox_inches='tight')
+
+  msg.reply_image('foo.png')
+embed()
